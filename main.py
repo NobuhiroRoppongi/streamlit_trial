20b2465c9fb0feccd1a5cfa900b4fc36362828a9
NobuhiroRoppongi <142144722+NobuhiroRoppongi@users.noreply.github.com>
2024-05-05 22:33:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ x = 10
 'x: ', x 
 st.radio("好きなマイケルは？", ('ジャクソン', 'ジョーダン', 'ホフマン'))
 uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
-# if uploaded_file is not None:
-#     df = pd.read_csv(
-#         uploaded_file,
-#         sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
-#         engine='python',
-#         na_values='-',
-#         header=None)
+if uploaded_file is not None:
+    df = pd.read_csv(
+        uploaded_file,
+        sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
+        engine='python',
+        na_values='-',
+        header=None)
 
 # st.markdown('### アクセスログ（先頭5件）')
 # st.write(df.head(5))
