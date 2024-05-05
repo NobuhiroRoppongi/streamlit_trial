f6553e6e29bebb14cbab872f58b063df37c9a4ba
NobuhiroRoppongi <142144722+NobuhiroRoppongi@users.noreply.github.com>
2024-05-05 13:04:38 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 # Setting up page link 
 st.set_page_config(page_title="Nobs Sample", page_icon='page_logo.png')
-st.title("Multiple OSS Access Log Analyzer")
+st.title("Nobs Sample analysis tool")
 
 df = pd.DataFrame({'col1': [1,2,3]})
 x = 10
