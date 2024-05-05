6df9d968ea646c451ba062a4ce5949d4de461c8c
NobuhiroRoppongi <142144722+NobuhiroRoppongi@users.noreply.github.com>
2024-05-05 11:51:03 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import streamlit as st
 import pandas as pd
+import streamlit as st
 
 df = pd.DataFrame({'col1': [1,2,3]})
 x = 10
 'x: ', x 
 st.radio("好きなマイケルは？", ('ジャクソン', 'ジョーダン', 'ホフマン'))
 
+uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
 if uploaded_file is not None:
     df = pd.read_csv(
         uploaded_file,
