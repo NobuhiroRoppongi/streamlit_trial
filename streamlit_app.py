55b366bfa102851a4f4486e6af913ef3c452f2f6
NobuhiroRoppongi <142144722+NobuhiroRoppongi@users.noreply.github.com>
2024-05-05 11:50:31 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,16 @@
 import pandas as pd
 import streamlit as st
+import pandas as pd
 
 df = pd.DataFrame({'col1': [1,2,3]})
 x = 10
 'x: ', x 
 st.radio("好きなマイケルは？", ('ジャクソン', 'ジョーダン', 'ホフマン'))
+
+if uploaded_file is not None:
+    df = pd.read_csv(
+        uploaded_file,
+        sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
+        engine='python',
+        na_values='-',
+        header=None)
